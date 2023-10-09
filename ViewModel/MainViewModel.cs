d7656004ffc6310c520643f2c6953525e8bf21b1
Servan42 <charlot.servan@gmail.com>
2023-10-10 00:51:05 +0200

    wip: Adding the frontend

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        WeightFormViewModel weightFormViewModel;
+
+        public MainViewModel()
+        {
+            this.weightFormViewModel = new();
+        }
+
+        public WeightFormViewModel WeightFormViewModel { get => weightFormViewModel; set => weightFormViewModel = value; }
+    }
+}
---
+++ ViewModel/WeightFormViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel
+{
+    public class WeightFormViewModel : ViewModelBase
+    {
+        private int cyclistWeight;
+        private int bikeWeight;
+
+        public int? CyclistWeight
+        {
+            get => cyclistWeight == 0 ? null : cyclistWeight;
+            set
+            {
+                cyclistWeight = value ?? 0;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(TotalWeight));
+            }
+        }
+
+        public int? BikeWeight
+        {
+            get => bikeWeight == 0 ? null : bikeWeight;
+            set
+            {
+                bikeWeight = value ?? 0;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(TotalWeight));
+            }
+        }
+
+        public string TotalWeight => $"Total: {cyclistWeight + bikeWeight}kg";
+    }
+}
